5b6c42193ee2c491af2aceb62fe6e8cfdd4153fb
Daksh <sethi.daksh06@gmail.com>
2026-06-30 21:57:15 +0530

    Time: 13 ms (29.51%) | Memory: 9.5 MB (3.83%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.c
@@ -0,0 +1,17 @@
+int numberOfSubstrings(char *s) {
+    int count[3] = {0};
+    int left = 0, ans = 0;
+    int n = strlen(s);
+
+    for (int right = 0; right < n; right++) {
+        count[s[right] - 'a']++;
+
+        while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+            ans += n - right;
+            count[s[left] - 'a']--;
+            left++;
+        }
+    }
+
+    return ans;
+}
